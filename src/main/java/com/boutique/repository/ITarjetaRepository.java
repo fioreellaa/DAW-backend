6e04c4bed7956671adc18909e7c88b7a383acfca
Judith14D <Judith.ramos.supanta@gmail.com>
2024-10-09 00:35:24 -0500

    eliminación Tarjeta

--- src/main/java/com/boutique/repository/ITarjetaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boutique.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.boutique.model.Tarjeta;
-
-public interface ITarjetaRepository  extends JpaRepository<Tarjeta, Integer> {
-
-}
